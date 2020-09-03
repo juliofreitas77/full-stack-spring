06da59a135a441098056393f3ed9641514d13a79
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-03 17:19:01 -0300

    Projeto Criado

---
+++ src/main/java/com/julio/modconceitual/ModconceitualApplication.java
@@ -0,0 +1,13 @@
+package com.julio.modconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModconceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julio/modconceitual/ModconceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio.modconceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModconceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
