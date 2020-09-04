d8be8d42825491c970440f017652c6dd50f064af
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-03 23:15:39 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/julio/modconceitual/services/CategoriaService.java
+++ src/main/java/com/julio/modconceitual/services/CategoriaService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.julio.modconceitual.domain.Categoria;
 import com.julio.modconceitual.repositories.CategoriaRepository;
+import com.julio.modconceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
-	
-	public Categoria buscar(Integer id) {
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() ->  new ObjectNotFoundException(
+				"Objeto Nao encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
