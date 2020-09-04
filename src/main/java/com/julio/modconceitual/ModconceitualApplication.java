89a8283a52e8dc62dbba0a8c914ece6866e5a997
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-03 22:00:39 -0300

    Criado operacao de instanciaçao

--- src/main/java/com/julio/modconceitual/ModconceitualApplication.java
+++ src/main/java/com/julio/modconceitual/ModconceitualApplication.java
@@ -1,13 +1,35 @@
 package com.julio.modconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModconceitualApplication {
+import com.julio.modconceitual.domain.Categoria;
+import com.julio.modconceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModconceitualApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModconceitualApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
