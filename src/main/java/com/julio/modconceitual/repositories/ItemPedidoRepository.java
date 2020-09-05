b1fed1c860c132752b20c44fa5dd306a1642750b
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-05 14:39:46 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/julio/modconceitual/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.julio.modconceitual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.julio.modconceitual.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
