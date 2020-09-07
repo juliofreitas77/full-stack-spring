da221b0f92bfc3c8717822569bdfaa642d8c865c
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-07 14:12:37 -0300

    Paginação com parametros opcionais

--- src/main/java/com/julio/modconceitual/services/CategoriaService.java
+++ src/main/java/com/julio/modconceitual/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.julio.modconceitual.domain.Categoria;
@@ -41,8 +44,13 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir categorias que possuem produtos!");
 		}
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 }
