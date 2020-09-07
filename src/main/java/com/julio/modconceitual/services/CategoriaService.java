ab68657681cb945d35b9cd8fc765d8d2d9ab3f19
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-07 12:00:46 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/julio/modconceitual/services/CategoriaService.java
+++ src/main/java/com/julio/modconceitual/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.julio.modconceitual.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.julio.modconceitual.domain.Categoria;
 import com.julio.modconceitual.repositories.CategoriaRepository;
+import com.julio.modconceitual.services.exceptions.DataIntegrityException;
 import com.julio.modconceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -17,16 +19,25 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElseThrow(() ->  new ObjectNotFoundException(
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto Nao encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir categorias que possuem produtos!");
+		}
 	}
 }
