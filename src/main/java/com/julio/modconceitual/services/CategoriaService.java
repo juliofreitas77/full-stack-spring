95e656e676cdf4477c4fe739359ea0e32a15e568
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-07 12:17:29 -0300

    Listando categorias com DTO

--- src/main/java/com/julio/modconceitual/services/CategoriaService.java
+++ src/main/java/com/julio/modconceitual/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.julio.modconceitual.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +40,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir categorias que possuem produtos!");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
 	}
 }
