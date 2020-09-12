1236efc32ddd3e781cf839250603bf8076369479
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-11 23:39:52 -0300

    Vaidaçao de inserçao de CPF/CNPJ na inserçao de cliente

---
+++ src/main/java/com/julio/modconceitual/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,41 @@
+package com.julio.modconceitual.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.julio.modconceitual.domain.enums.TipoCliente;
+import com.julio.modconceitual.dto.ClienteNewDTO;
+import com.julio.modconceitual.resources.exception.FieldMessage;
+import com.julio.modconceitual.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
