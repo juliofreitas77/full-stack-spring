cf7548185074602cb1c901b8e6f866463779f604
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-12 14:31:26 -0300

    Busca produtos por nome e categorias

---
+++ src/main/java/com/julio/modconceitual/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.julio.modconceitual.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.julio.modconceitual.domain.Categoria;
+import com.julio.modconceitual.domain.Produto;
+import com.julio.modconceitual.repositories.CategoriaRepository;
+import com.julio.modconceitual.repositories.ProdutoRepository;
+import com.julio.modconceitual.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repository.findById(id);
+		return obj.orElseThrow(() ->  new ObjectNotFoundException(
+				"Objeto Nao encontrado! Id:" + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repository.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}
+}
