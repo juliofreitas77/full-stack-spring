d188f428c0c309dd61221ba4673c65bab348a1cd
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-13 14:40:45 -0300

    Criando profile de Dev

--- src/main/java/com/julio/modconceitual/ModconceitualApplication.java
+++ src/main/java/com/julio/modconceitual/ModconceitualApplication.java
@@ -7,10 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ModconceitualApplication implements CommandLineRunner {
 
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModconceitualApplication.class, args);
 	}
+	
 
 	@Override
 	public void run(String... args) throws Exception {
