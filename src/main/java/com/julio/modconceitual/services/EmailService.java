a926014b85c7fd42ec701ab0efef7a77e291eab0
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-15 17:57:48 -0300

    Email HTML

--- src/main/java/com/julio/modconceitual/services/EmailService.java
+++ src/main/java/com/julio/modconceitual/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.julio.modconceitual.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.julio.modconceitual.domain.Pedido;
@@ -7,7 +9,13 @@ import com.julio.modconceitual.domain.Pedido;
 public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido obj);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendHtmlEmail(MimeMessage msg);
+	
+
 
 }
