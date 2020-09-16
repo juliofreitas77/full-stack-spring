b6bf9684e4471adecd1fad0759bc0d40ec19af96
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-16 18:16:13 -0300

    Configuracao spring security

--- src/main/java/com/julio/modconceitual/services/EmailService.java
+++ src/main/java/com/julio/modconceitual/services/EmailService.java
@@ -7,15 +7,9 @@ import org.springframework.mail.SimpleMailMessage;
 import com.julio.modconceitual.domain.Pedido;
 
 public interface EmailService {
-	
 	void sendOrderConfirmationEmail(Pedido obj);
-
-	void sendOrderConfirmationHtmlEmail(Pedido obj);
-	
 	void sendEmail(SimpleMailMessage msg);
 	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	void sendHtmlEmail(MimeMessage msg);
-	
-
-
 }
