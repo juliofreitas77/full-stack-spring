160fde679016f59d77f1bcae330c97c5841d6af4
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-18 21:02:19 -0300

    Implementando Autorizacao

--- src/main/java/com/julio/modconceitual/config/SecurityConfig.java
+++ src/main/java/com/julio/modconceitual/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.julio.modconceitual.security.JWTAuthenticationFilter;
+import com.julio.modconceitual.security.JWTAuthorizationFilter;
 import com.julio.modconceitual.security.JWTUtil;
 
 @Configuration
@@ -51,6 +52,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
