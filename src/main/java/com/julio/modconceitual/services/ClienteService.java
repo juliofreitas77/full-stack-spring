4b48bd80a10ef206979a4bfb391f54700268ad91
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-22 17:25:44 -0300

    Endpoint Buscando cliente por email

--- src/main/java/com/julio/modconceitual/services/ClienteService.java
+++ src/main/java/com/julio/modconceitual/services/ClienteService.java
@@ -130,4 +130,19 @@ public class ClienteService {
 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
+	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Cliente obj = repository.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
 }
