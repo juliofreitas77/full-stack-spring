ea9c2d39d3cb7e34bacd98551db109e409eac232
Julio Cesar <julio.si.freitas@gmail.com>
2020-09-22 12:05:26 -0300

    Tratando exceptions adequadamente.

--- src/main/java/com/julio/modconceitual/services/S3Service.java
+++ src/main/java/com/julio/modconceitual/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.julio.modconceitual.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -29,11 +30,11 @@ public class S3Service {
 	public URI uploadFile(MultipartFile multipartFile) {
 		try {
 			String fileName = multipartFile.getOriginalFilename();
-			InputStream is = multipartFile.getInputStream();
+			InputStream is = multipartFile.getInputStream();	
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO");
+			throw new FileException("Erro de IO");
 		}
 	}
 
@@ -46,7 +47,7 @@ public class S3Service {
 			LOG.info("Upload finalizado");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
